573ae897a7647a32977290765b1d1998c14d6637
kacperchm <chmuraka1998@gmail.com>
2023-05-13 12:39:17 +0200

    Initial commit

---
+++ src/test/java/com/example/pizzamarket/PizzaMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pizzamarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
